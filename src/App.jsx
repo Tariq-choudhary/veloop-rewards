e216bc77e7408a40f9393f99e0fb4e88b47cb533
Tariq-choudhary <choudharytariq2004@gmail.com>
2026-09-14 13:36:05 +0530

    Fix GamesPage folder case

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/common/Header.jsx';
 import LoginModal from './components/common/LoginModal.jsx';
 
-import GamesPage from './pages/Games/GamesPage.jsx';
+import GamesPage from './pages/games/GamesPage.jsx';
 import GameLoadingPage from './pages/games/GameLoadingPage.jsx';
 import GameHomePage from './pages/games/GameHomePage.jsx';
 import GameGuidePage from './pages/games/GameGuidePage.jsx';
@@ -21,20 +21,46 @@ export default function App() {
     <>
       <Header />
       <LoginModal />
+
       <Routes>
         <Route path="/" element={<Navigate to="/games" replace />} />
+
         <Route path="/games" element={<GamesPage />} />
-        <Route path="/games/:gameId/loading" element={<GameLoadingPage />} />
-        <Route path="/games/:gameId/guide" element={<GameGuidePage />} />
-        <Route path="/games/:gameId/play" element={<GamePlayPage />} />
-        <Route path="/games/:gameId/result" element={<GameResultPage />} />
-        <Route path="/games/:gameId" element={<GameHomePage />} />
+        <Route
+          path="/games/:gameId/loading"
+          element={<GameLoadingPage />}
+        />
+        <Route
+          path="/games/:gameId/guide"
+          element={<GameGuidePage />}
+        />
+        <Route
+          path="/games/:gameId/play"
+          element={<GamePlayPage />}
+        />
+        <Route
+          path="/games/:gameId/result"
+          element={<GameResultPage />}
+        />
+        <Route
+          path="/games/:gameId"
+          element={<GameHomePage />}
+        />
+
         <Route path="/giveaways" element={<GiveawaysPage />} />
-        <Route path="/giveaway/:slug" element={<GiveawayDetailsPage />} />
-        <Route path="/level" element={<LevelDashboardPage />} />
+        <Route
+          path="/giveaway/:slug"
+          element={<GiveawayDetailsPage />}
+        />
+
+        <Route
+          path="/level"
+          element={<LevelDashboardPage />}
+        />
         <Route path="/rewards" element={<RewardsPage />} />
         <Route path="/redeem" element={<RedeemPage />} />
         <Route path="/withdraw" element={<WithdrawPage />} />
+
         <Route path="*" element={<Navigate to="/games" replace />} />
       </Routes>
     </>
